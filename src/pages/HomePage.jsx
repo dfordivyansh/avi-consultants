62262fa4f2fdb073fd94b8335723e16d2f92f00f
Divyansh Srivastava <dfordivyansh3@gmail.com>
2025-11-20 01:19:04 +0530

    first commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,30 +1,13 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
-import WhatWeDo from "../components/WhatWeDo";
-import MoreServices from "../components/MoreServices";
-import LetsTalk from "../components/LetsTalk";
-import Footer from "../components/Footer";
-import ProfitAuditSteps from "../components/ProfitAuditSteps";
-import ROIGuarentee from "../components/ROIGuarentee";
-import Testimonials from "../components/Testimonials";
-import Numbers from "../components/Numbers";
-import Referrals from "../components/Referrals";
+
 
 const HomePage = () => {
   return (
     <>
       <Navbar />
       <Hero />
-      <WhatWeDo />
-      <MoreServices />
-      <ProfitAuditSteps />
-      <ROIGuarentee />
-      <Testimonials />
-      <Numbers />
-      <Referrals />
-      <LetsTalk />
-      <Footer />
     </>
   );
 };
